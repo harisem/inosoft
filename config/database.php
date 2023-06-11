3dfb2ee3e0547cf0c4cae531dfb6c55312a51252
harisem <harmuhammadis@gmail.com>
2023-06-11 19:28:52 +0700

    update env files

--- config/database.php
+++ config/database.php
@@ -93,8 +93,8 @@ return [
 
         'mongodb' => [
             'driver' => 'mongodb',
-            'dsn' => env('DB_URI'),
-            'database' => env('DB_DATABASE', 'vehicles'),
+            'dsn' => env('MONGO_DB_URI'),
+            'database' => env('MONGO_DB_DATABASE'),
         ],
 
     ],
